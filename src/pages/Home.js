67bc71528cf8bce09c06e0d1009154ad14b5c394
gaulbaram0111 <gaulbaram0111@gmail.com>
2023-07-10 17:10:04 +0900

    230710

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,41 +1,13 @@
-import { useSearchParams } from "react-router-dom";
-import Button from "../component/Buttons";
+import Editor from "../component/Editor";
 
 const Home = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
-  console.log(searchParams.get("sort"));
   return (
     <div>
-      <div>Home 페이지 입니다.</div>
-      <div>
-        <Button
-          text={"기본버튼"}
-          onClick={() => {
-            alert("기본");
-          }}
-        />
-        <Button
-          text={"기본버튼2"}
-          type={"default"}
-          onClick={() => {
-            alert("기본2");
-          }}
-        />
-        <Button
-          text={"긍정"}
-          type={"positive"}
-          onClick={() => {
-            alert("positive");
-          }}
-        />
-        <Button
-          text={"부정"}
-          type={"negative"}
-          onClick={() => {
-            alert("negative");
-          }}
-        />
-      </div>
+      <Editor
+        onSubmit={() => {
+          alert("작성 완료 버튼을 클릭했음");
+        }}
+      />
     </div>
   );
 };
--- src/util.js
+++ src/util.js
@@ -21,3 +21,23 @@ export const getEmotionImgById = (emotionId) => {
       return null;
   }
 };
+
+export const getFormattedDate = (targetDate) => {
+  let year = targetDate.getFullYear();
+  let month = targetDate.getMonth() + 1;
+  let date = targetDate.getDate();
+
+  if (month < 10) {
+    month = `0${month}`;
+  }
+
+  return `${year}-${month}-${date}`;
+};
+
+export const emotionList = [
+  { id: 1, name: "완전 좋음", img: getEmotionImgById(1) },
+  { id: 2, name: "좋음", img: getEmotionImgById(2) },
+  { id: 3, name: "그럭저럭", img: getEmotionImgById(3) },
+  { id: 4, name: "나쁨", img: getEmotionImgById(4) },
+  { id: 5, name: "완전 나쁨", img: getEmotionImgById(5) },
+];
