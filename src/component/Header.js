c71cae82a7b0840c2b370f1b5ab2dcd3749557b2
gaulbaram0111 <gaulbaram0111@gmail.com>
2023-07-11 17:41:27 +0900

    230711

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,9 +1,11 @@
+import "./Header.css";
+
 const Header = ({ title, leftChild, rightChild }) => {
   return (
     <div className="Header">
-      <div className="Header_left">{leftChild}</div>
-      <div className="Header_title">{title}</div>
-      <div className="Header_right">{rightChild}</div>
+      <div className="header_left">{leftChild}</div>
+      <div className="header_title">{title}</div>
+      <div className="header_right">{rightChild}</div>
     </div>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,26 @@
-import Editor from "../component/Editor";
-
+import { useState } from "react";
+import Button from "../component/Button";
+import Header from "../component/Header";
 const Home = () => {
-  return <div>Home 페이지 입니다.</div>;
+  const [pivotDate, setPivotDate] = useState(new Date());
+  const headerTitle = `${pivotDate.getFullYear()}년 ${
+    pivotDate.getMonth() + 1
+  }월`;
+  const onIncreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
+  };
+  const onDecreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1));
+  };
+
+  return (
+    <div>
+      <Header
+        title={headerTitle}
+        leftChild={<Button text={"<"} onClick={onDecreaseMonth} />}
+        rightChild={<Button text={">"} onClick={onIncreaseMonth} />}
+      />
+    </div>
+  );
 };
 export default Home;
